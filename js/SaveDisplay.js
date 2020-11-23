e2f1ec950cb9e59dbfb6800832bdae04865a2ee2
NoahTV <noahtv@usca.edu>
2020-11-23 01:36:28 -0500

    1 file modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,24 +1,37 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyCjRCnl-0ohz1MahXwZ7pnYNU0oaUWqgaA",
+  authDomain: "grocerylist-290a6.firebaseapp.com",
+  databaseURL: "https://grocerylist-290a6.firebaseio.com",
+  projectId: "grocerylist-290a6",
+  storageBucket: "grocerylist-290a6.appspot.com",
+  messagingSenderId: "341204536159",
+  appId: "1:341204536159:web:1b8576bf2ccfed115384ea",
+  measurementId: "G-PS1ZDC8DKG"};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
 $(".sampleSurvey input[type='submit']").click(function(e){
-e.preventDefault();
+  e.preventDefault();
 
-var data = $("form").serializeArray();
-console.log(data);
+  var data = $("form").serializeArray();
+  console.log(data);
+  var inputJson = {};
+  for(var i = 0; i < data.length; i++) {
+    var name = data[i] ["name"];
+    var value = data[i] ["value"];
 
-var inputJson = {};
-for(var i = 0; i < data.length; i++) {
-  var name = data[i] ("comm");
-  var value = data[i] ("value");
+    inputJson[name] = value;
+  }
+  console.log(inputJson)
 
-  inputJson(name) = value;
-}
-console.log(inputJson)
+ firebase
+    .firestore()
+    .collection("surveyList")
+    .add(inputJson);
 
+  $("form")[0].reset();
 });
 // update the result in table
